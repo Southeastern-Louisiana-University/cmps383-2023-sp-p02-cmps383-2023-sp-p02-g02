112b4e743e50167bb88e5e5c9b47a0a03e80daa8
Fastcat31 <jeremy.clay@selu.edu>
2023-02-07 17:05:47 -0600

    Added Seeded Data and Dbset added

--- SP23.P02.Web/Data/SeedHelper.cs
+++ SP23.P02.Web/Data/SeedHelper.cs
@@ -15,12 +15,36 @@ public static class SeedHelper
         {
             for (int i = 0; i < 3; i++)
             {
-                dataContext.Set<TrainStation>()
-                    .Add(new TrainStation
-                    {
-                        Name = "Hammond",
-                        Address = "1234 Place st"
-                    });
+                var trainstations = new List<TrainStation>
+            {
+                new TrainStation
+                {
+                    Name = "Hammond Train",
+                    Address = "123 Hammond Dr"
+                },
+                new TrainStation
+                {
+                    Name = "New Orleans Train",
+                    Address = "123 New Orleans Dr"
+                },
+                new TrainStation
+                {
+                    Name = "Baton Rouge Train",
+                    Address = "123 Baton Rouge Dr"
+                },
+                new TrainStation
+                {
+                    Name = "Lafayette Train",
+                    Address = "123 Lafayette Dr"
+                },
+            };
+
+                //loops through the seeded data  
+                foreach (var trainstation in trainstations)
+                {
+                    dataContext.TrainStations.Add(trainstation);
+                }
+
             }
 
             await dataContext.SaveChangesAsync();
