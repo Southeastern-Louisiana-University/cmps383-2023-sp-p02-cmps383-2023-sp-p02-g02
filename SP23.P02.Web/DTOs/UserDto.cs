aa9653de477fbdf2d8aee377ddfe9fab02c763a1
Fastcat31 <jeremy.clay@selu.edu>
2023-02-07 14:32:02 -0600

    Created Dtos

---
+++ SP23.P02.Web/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SP23.P02.Web.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string[] Roles { get; set; } 
+    }
+}
