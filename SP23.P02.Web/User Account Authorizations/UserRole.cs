654215840a2454bdcac553715fd3f646057be777
Fastcat31 <jeremy.clay@selu.edu>
2023-02-08 20:19:43 -0600

    User Identity and Roles

---
+++ SP23.P02.Web/User Account Authorizations/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Data;
+
+namespace SP23.P02.Web.User_Account_Authorizations
+{
+    public class UserRole : IdentityUserRole<int>
+    {      
+            public virtual Role? Role { get; set; }
+            public virtual User? User { get; set; }
+        
+
+
+    }
+}
