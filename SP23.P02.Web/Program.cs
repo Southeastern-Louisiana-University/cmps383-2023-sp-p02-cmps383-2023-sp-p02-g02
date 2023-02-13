daadc333539731c6418717c5f1a5db087d0ee8ff
Kaitlyn Hayes <kaitlyn.hayes-2@selu.edu>
2023-02-12 21:30:11 -0600

    react web setup
    getting react web setup

--- SP23.P02.Web/Program.cs
+++ SP23.P02.Web/Program.cs
@@ -32,7 +32,26 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(routeBuilder =>
+{
+    routeBuilder.MapControllers();
+});
+
+app.UseStaticFiles();
+
+app.UseSpa(spaBuilder =>
+{
+    spaBuilder.Options.SourcePath = "ClientApp";
+    if (app.Environment.IsDevelopment())
+    {
+        spaBuilder.UseProxyToSpaDevelopmentServer("https://localhost:3000");
+    }
+});
+
+
+//app.MapControllers();
 
 app.Run();
 
